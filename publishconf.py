a7925e76dc2de20fe47663217a767a9c14f82253
JhonyVilla <jhony_villa@outlook.com>
2017-05-25 16:48:30 -0500

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -2,23 +2,53 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
-
-import os
 import sys
-sys.path.append(os.curdir)
-from pelicanconf import *
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
+AUTHOR = 'Fluid'
+SITENAME = 'Blog de Fluid'
 SITEURL = 'https://lordjhony.github.io'
-RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+PATH = 'content'
 
-DELETE_OUTPUT_DIRECTORY = True
+TIMEZONE = 'America/Bogota'
 
-# Following items are often useful when publishing
+DEFAULT_LANG = 'Spanish'
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Formating URLs
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+
+# Plugins
+PLUGIN_PATHS = 'pelican-plugins'
+PLUGINS = ['asciidoc_reader','assets','neighbors','share_post','related_posts','disqus_static']
+
+# Discus
+# DISQUS_SITENAME = 'lordjhony.disqus.com'
+
+# theme
+THEME = "theme/pelican-clean-blog"
+
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
